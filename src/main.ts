5553dfd5258028b340f96027d2860901da486da5
moreal <dev.moreal@gmail.com>
2023-12-05 17:04:59 +0900

    Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('NineChronicles Relay Bridge API')
+    .setDescription(
+      'The API to access NineChronicles Relay Bridge event histories',
+    )
+    .setVersion('alpha')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
