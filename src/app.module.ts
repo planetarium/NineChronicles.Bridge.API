1e081f47d8da5e51c65c7f8c744a27cceae8191a
moreal <dev.moreal@gmail.com>
2023-12-05 17:07:28 +0900

    Remove example endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { EventModule } from './event/event.module';
 
 @Module({
   imports: [PrismaModule, EventModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
